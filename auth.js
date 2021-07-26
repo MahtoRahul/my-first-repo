c7928be642b0ff7efbf00437d7f0bb90a1ccaf72
MahtoRahul <rahulmahato7484@gmail.com>
2021-07-26 19:41:30 +0530

    auth.js changes

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ const DB = process.env.DATABASE;
 mongoose.connect(DB,{
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: false,
     useFindAndModify: false
     
  }).then( () => {
